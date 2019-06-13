6cb7523b51646eea7c1c52b78dc6b4d3a5b8e440
punchjidapa <50682524+punchjidapa@users.noreply.github.com>
2019-06-13 15:15:53 +0700

    Update ARDUINO_UNO_W.ino

--- W/ARDUINO_UNO_W/ARDUINO_UNO_W.ino
+++ W/ARDUINO_UNO_W/ARDUINO_UNO_W.ino
@@ -4,7 +4,6 @@
 Servo my2servo;
 Servo my3servo;
 
-
 int pos = 0;
 
 #define trigPin  A5
@@ -29,10 +28,6 @@ const int enPin   = 5;
 byte ena = 6; //oramgedriveแดง
 byte in1 = 7; //greendriveแดง
 byte in2 = 8; //ขี้ม้าdriveแดง
-
-//int hallSensorPin = 2;
-//int ledPin =  13;
-//int state = 0;
 
 #include <HX711.h>
 #include "HX711.h"
@@ -60,19 +55,13 @@ HX711 scale(DOUT, CLK);
 SoftwareSerial NodeSerial(12, 13);
 int positionData = 0;
 
-//-----------------------------------------------------------------------------------------
-
 void setup()
 {
-  //  motorStep.setSpeed(30);
-
-  //myservo.attach(9);
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
   Serial.begin(9600);
   NodeSerial.begin(9600);
-  //Serial.println("Load Cell");
   scale.set_scale(calibration_factor);
   scale.set_offset(zero_factor);
 
@@ -86,30 +75,12 @@ void setup()
   pinMode( in1, OUTPUT );
   pinMode( in2, OUTPUT );
 
-  /* pinMode(ledPin, OUTPUT);
-    pinMode(hallSensorPin, INPUT);
-    digitalWrite(enPin, LOW);
-    // Set Dir to Home switch
-    digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction*/
-
   my2servo.attach(11); //ที่เปิด
   my3servo.attach(10); //ที่ปัด
 }
-//void(*resetFunc)(void)=0;
 
 void loop()
 {
-
-  /*state = digitalRead(hallSensorPin);
-    Serial.println(state);
-    digitalRead(dirPin)==  HIGH;
-    digitalWrite(stepPin, HIGH);
-    digitalRead(enPin) ==HIGH;
-    delayMicroseconds(2000);
-    //digitalWrite(stepPin, LOW);
-    //delayMicroseconds(2000);
-    {
-    if ( state == LOW )*/
   {
     //motorStep(1);
     digitalRead(enPin) == LOW;
@@ -121,6 +92,7 @@ void loop()
     delay(100);
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH); // Reads the echoPin
+    
     // Calculate the distance (Garbage in the bin)
     // lid of the bin open
     distance = duration * 0.034 / 2;
@@ -129,24 +101,19 @@ void loop()
     {
       if (distance < 45 && distance >= 0) {
         {
-          //------------------------------------------------------------------------------------------------------------------------------------------------
-
           analogWrite( ena, 300 );
-
           digitalWrite( in1, HIGH );
           digitalWrite( in2, LOW );
           Serial.print("Uppppppp");
           delay(10000);
-
+          
           analogWrite( ena, 0 );
-
           digitalWrite( in1, LOW );
           digitalWrite( in2, LOW );
           Serial.print("hold on");
           delay(4000);
-
+          
           analogWrite( ena, -300 );
-
           digitalWrite( in1, LOW );
           digitalWrite( in2, HIGH );
           Serial.print("downnnnnn");
@@ -155,18 +122,11 @@ void loop()
           analogWrite( ena, 0 );
           digitalWrite( in1, LOW );
           digitalWrite( in2, LOW );
-          // Serial.println("Stop");
-
-          //------------------------------------------------------------------------------------------------------------------------------------------------
         }
 
         //waste separation process****************************************************
         monitoring = analogRead(Dout);
         metalDetected = (float) monitoring * 100 / 1024.0;
-        //Serial.print("14CORE METAL DETECTOR TEST");
-        //delay(500);
-        //Serial.print("Initializing Proximity Sensor");
-        //delay(500);
         Serial.print("Metal is Proximited = ");
         Serial.print(metalDetected);
         Serial.println("%");
@@ -180,31 +140,17 @@ void loop()
           Serial.print(weight + offset, DEC_POINT);
           Serial.println(" kg");
           Serial.println(weight + offset, 4);
-          //if ((weight+offset > 0.005 && weight+offset <= 0.016) || (weight+offset > 0.0161 && weight+offset <= 0.030))
-          //if ((0.005 < weight+offset < 0.009) || (0.016 < weight+offset <=0.024))
           if (weight + offset <= 0.000) {
             Serial.println("Put your trash");
             Serial.println("----------------------------------------------------------");
             delay(5000);
           }
 
-          //else if ((weight + offset <= 0.0115) || (weight + offset >= 0.0150 && weight + offset <= 0.020 ))
-          // else if (weight + offset <= 0.017)
           else if (weight + offset <= 0.02)
 
           {
-
-            /*positionData = 3;                       /////////////////////////////////
-            Serial.println(positionData);
-            //        Serial.println(analogData);
-            if (NodeSerial.available() > 0)
-            {
-              NodeSerial.write(positionData);
-            }*/
-
             Serial.println("CARTON");
             Serial.println("----------------------------------------------------------");
-            //delay(5000);
 
             my3servo.write(90);
             delay(1000);
@@ -215,7 +161,6 @@ void loop()
 
             //180 degree
             digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
             for (int x = 0; x < 1500; x++)
             {
               digitalWrite(stepPin, HIGH);
@@ -229,7 +174,7 @@ void loop()
             my2servo.write(90);
             delay(2000);
 
-            positionData = 3;                       /////////////////////////////////
+            positionData = 3;                      
             Serial.println(positionData);
             //        Serial.println(analogData);
             if (NodeSerial.available() > 0)
@@ -238,7 +183,6 @@ void loop()
             }
             
             digitalWrite(dirPin, LOW); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
             for (int x = 0; x < 1500; x++)
             {
               digitalWrite(stepPin, HIGH);
@@ -246,27 +190,14 @@ void loop()
               digitalWrite(stepPin, LOW);
               delayMicroseconds(2000);
             }
-            //delay(10000); // One second delay
           }
 
           else
           {
-            /*positionData = 4;                       /////////////////////////////////
-            Serial.println(positionData);
-            //        Serial.println(analogData);
-            if (NodeSerial.available() > 0)
-            {
-              NodeSerial.write(positionData);
-            }*/
 
             Serial.println("CAN");
             Serial.println("----------------------------------------------------------");
-            //  analogWrite( ena, 0 );
-            // digitalWrite( in1, LOW );
-            // digitalWrite( in2, LOW );
-            //  Serial.println("Stop");
-            //delay(500);
-
+            
             my3servo.write(90);
             delay(1000);
             my3servo.write(45);
@@ -276,7 +207,6 @@ void loop()
 
             //270 degree
             digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
             for (int x = 0; x < 2300; x++)
             {
               digitalWrite(stepPin, HIGH);
@@ -290,16 +220,14 @@ void loop()
             my2servo.write(90);
             delay(2000);
 
-            positionData = 4;                       /////////////////////////////////
+            positionData = 4;                       
             Serial.println(positionData);
-            //        Serial.println(analogData);
             if (NodeSerial.available() > 0)
             {
               NodeSerial.write(positionData);
             }
             
             digitalWrite(dirPin, LOW); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
             for (int x = 0; x < 2300; x++)
             {
               digitalWrite(stepPin, HIGH);
@@ -308,7 +236,6 @@ void loop()
               delayMicroseconds(2000);
             }
             delay(10000); // One second delay
-
           }
         }
 
@@ -326,23 +253,12 @@ void loop()
             Serial.println("----------------------------------------------------------");
             delay(5000);
           }
-          // else if (weight + offset > 0.12)
           else if (weight + offset > 0.03)
           {
-            /*positionData = 2;                       /////////////////////////////////
-            Serial.println(positionData);
-            //        Serial.println(analogData);
-            if (NodeSerial.available() > 0)
-            {
-              NodeSerial.write(positionData);
-            }*/
 
             Serial.println("GLASS");
             Serial.println("----------------------------------------------------------");
-            //delay(5000);
 
-            // my3servo.write(-60);
-            //delay(1000);
             my3servo.write(90);
             delay(1000);
             my3servo.write(45);
@@ -352,7 +268,6 @@ void loop()
 
             //0 degree
             digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
 
             for (int x = 0; x++;)
             {
@@ -368,9 +283,8 @@ void loop()
             my2servo.write(90);
             delay(2000);
 
-            positionData = 2;                       /////////////////////////////////
+            positionData = 2;                       
             Serial.println(positionData);
-            //        Serial.println(analogData);
             if (NodeSerial.available() > 0)
             {
               NodeSerial.write(positionData);
@@ -379,20 +293,11 @@ void loop()
           }
           else
           {
-            /*positionData = 1;                       /////////////////////////////////
-            Serial.println(positionData);
-            //        Serial.println(analogData);
-            if (NodeSerial.available() > 0)
-            {
-              NodeSerial.write(positionData);
-            }*/
 
             Serial.println("PLASTIC BOTTLE");
             Serial.println("----------------------------------------------------------");
             delay(500);
 
-            // my3servo.write(-60);
-            // delay(1000);
             my3servo.write(90);
             delay(1000);
             my3servo.write(45);
@@ -402,7 +307,6 @@ void loop()
 
             //90 degree
             digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
             for (int x = 0; x < 700; x++)
             {
               digitalWrite(stepPin, HIGH);
@@ -412,22 +316,20 @@ void loop()
             }
             delay(1000); // One second delay
 
-
             my2servo.write(-90);
             delay(2000);
             my2servo.write(90);
             delay(2000);
 
-            positionData = 1;                       /////////////////////////////////
+            positionData = 1;                     
             Serial.println(positionData);
-            //        Serial.println(analogData);
+
             if (NodeSerial.available() > 0)
             {
               NodeSerial.write(positionData);
             }
             
             digitalWrite(dirPin, LOW); // Enables the motor to move in a particular direction
-            // Makes 200 pulses for making one full cycle rotation
             for (int x = 0; x < 700; x++)
             {
               digitalWrite(stepPin, HIGH);
@@ -441,15 +343,6 @@ void loop()
       }
     }
   }
-
-  /*  else {
-      digitalRead(dirPin)== HIGH;
-      digitalWrite(stepPin, HIGH);
-       digitalWrite(enPin, HIGH);
-      delayMicroseconds(2000);
-      //digitalWrite(stepPin, LOW);
-     // delayMicroseconds(2000);
-    }*/
 }
 
 
